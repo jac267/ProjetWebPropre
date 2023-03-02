e2d7632e77f2e87c732ad8bf882574fe5c850d67
jac267 <jackpotchap@gmail.com>
2023-03-02 10:41:32 -0500

    grid systeme

---
+++ script/animatedBackground.js
@@ -0,0 +1,24 @@
+// code trouver sur : https://www.vantajs.com/
+
+window.addEventListener("DOMContentLoaded", () => {
+  VANTA.FOG({
+    el: "#vanta",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.0,
+    minWidth: 200.0,
+    highlightColor: 0xe4e9ff,
+    midtoneColor: 0x4d34d1,
+    lowlightColor: 0x7a66db,
+    baseColor: 0xa0a51,
+    blurFactor: 0.83,
+    speed: 3.3,
+    zoom: 0.5,
+  });
+  setTimeout(() => {
+    const main = document.querySelector("main");
+    main.style.opacity = 1;
+    main.style.filter = "blur(0px)";
+  }, 1000);
+});
