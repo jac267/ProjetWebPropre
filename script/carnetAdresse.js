0fb6a788ac0fde1aa1f48095704b52b927536329
jac267 <jackpotchap@gmail.com>
2023-03-02 18:24:16 -0500

    carnet addresse fonctionnel

--- script/carnetAdresse.js
+++ script/carnetAdresse.js
@@ -1,11 +1,61 @@
 function updateList(selected) {
-  myElement = document.getElementsByClassName("carnet-adresse-list")[0];
+  myElement = document.getElementById("carnet-adresse-list");
 
   for (const child of myElement.children[0].children) {
     if (child === selected) {
-      child.className = "selected";
+      if (child.className === "selected") {
+        child.className = "";
+      } else {
+        child.className = "selected";
+      }
     } else {
       child.className = "";
     }
   }
+}
+
+function refreshList() {
+  let carnetDAdresse = JSON.parse(localStorage.getItem("carnetDAdresse"));
+
+  if (carnetDAdresse != null) {
+    myElement = document.getElementById("carnet-adresse-list");
+    myElement = myElement.children[0];
+    myElement.textContent = "";
+
+    for (let i = 0; i < carnetDAdresse.valeur.length; i++) {
+      const node = document.createElement("li");
+
+      node.innerHTML = carnetDAdresse["valeur"][i].Name;
+      node.setAttribute("onclick", "updateList(this)");
+      node.setAttribute("ondblclick", "open_mod(" + i + ")");
+      myElement.appendChild(node);
+    }
+  }
+}
+
+function triggerMod() {
+  myElement = document.getElementsByClassName("selected")[0];
+  var event = new MouseEvent("dblclick", {
+    view: window,
+    bubbles: true,
+    cancelable: true,
+  });
+  myElement.dispatchEvent(event);
+}
+
+function remove() {
+  myElement = document.getElementsByClassName("selected")[0];
+  const index = parseInt(
+    String(myElement.attributes.ondblclick.textContent)
+      .replace("open_mod(", "")
+      .replace(")", "")
+  );
+
+  console.log(index);
+  let carnetDAdresse = JSON.parse(localStorage.getItem("carnetDAdresse"));
+
+  carnetDAdresse.valeur.splice(index, 1);
+
+  localStorage.setItem("carnetDAdresse", JSON.stringify(carnetDAdresse));
+  refreshList();
 }
---
+++ script/popUpModifierCarnet.js
@@ -0,0 +1,50 @@
+function open_new() {
+  let popUp = document.getElementById("wraper-popup-modifier-carnet");
+  popUp.style.display = "block";
+}
+
+function open_mod(index) {
+  let carnetDAdresse = JSON.parse(localStorage.getItem("carnetDAdresse"))
+    .valeur[index];
+
+  document.getElementById("contact-name-field").value = carnetDAdresse.Name;
+  document.getElementById("contact-public-key-field").value =
+    carnetDAdresse.PublicKey;
+
+  let popUp = document.getElementById("wraper-popup-modifier-carnet");
+  popUp.style.display = "block";
+}
+
+function fermer() {
+  let popUp = document.getElementById("wraper-popup-modifier-carnet");
+  popUp.style.display = "none";
+
+  document.getElementById("contact-name-field").value = "";
+  document.getElementById("contact-public-key-field").value = "";
+}
+
+function sauvegarder() {
+  let localStorageSize = 0;
+  let carnetDAdresse = JSON.parse(localStorage.getItem("carnetDAdresse"));
+
+  if (carnetDAdresse != null) {
+    localStorageSize = carnetDAdresse.valeur.length;
+  }
+  console.log();
+  let dict_contact = {
+    Name: document.getElementById("contact-name-field").value,
+    PublicKey: document.getElementById("contact-public-key-field").value,
+  };
+
+  if (localStorageSize == 0) {
+    localStorage.setItem(
+      "carnetDAdresse",
+      '{"valeur":[' + JSON.stringify(dict_contact) + "]}"
+    );
+  } else {
+    carnetDAdresse.valeur[localStorageSize] = dict_contact;
+    localStorage.setItem("carnetDAdresse", JSON.stringify(carnetDAdresse));
+  }
+  refreshList();
+  fermer();
+}
