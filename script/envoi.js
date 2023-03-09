b57c8f8d7d5e5b6c52c9fe44f1a1f49f9272b490
CA-WillMc <122403149+CA-WillMc@users.noreply.github.com>
2023-03-09 10:42:51 -0500

    fr

--- script/envoi.js
+++ script/envoi.js
@@ -1,54 +1,59 @@
 /*Fonction qui va permettre d'envoyer un courriel au localstorage, elle va enregistrer les données des champs de texts et les sauvegarder en localStorage */
-function send(){
-    
-    
-    let titre_txt =document.getElementById("title-id").value;
-    let receiver_txt =document.getElementById("destinataire-id").value;
-    let content_txt =document.getElementById("content-id").value;
+function send() {
+  let titre_txt = document.getElementById("title-id").value;
+  let receiver_txt = document.getElementById("destinataire-id").value;
+  let content_txt = document.getElementById("content-id").value;
 
-    if(titre_txt.length == 0){alert("Veiller rajouter un titre")}
-    if(receiver_txt.length == 0){alert("Veiller rajouter un destinataire")}
-    if(content_txt.length == 0){alert("Veiller rajouter un message")}
-    
-    if(titre_txt.length && receiver_txt.length && content_txt.length != 0){
+  if (titre_txt.length == 0) {
+    alert("Veuillez ajouter un titre");
+  }
+  if (receiver_txt.length == 0) {
+    alert("Veuillez ajouter un destinataire");
+  }
+  if (content_txt.length == 0) {
+    alert("Veuillez ajouter un message");
+  }
 
-        document.getElementById("title-id").value = "";
-        document.getElementById("destinataire-id").value = "";
-        document.getElementById("content-id").value ="";
-        alert("Message envoyé avec succès");
-        
-        let email ={};
-        email.titre = titre_txt
-        email.destinataire = receiver_txt
-        email.contenu = content_txt
-        let emailstr = JSON.stringify(email);
-        console.log(emailstr)
-        
-        let value = localStorage.nombre;
+  if (titre_txt.length && receiver_txt.length && content_txt.length != 0) {
+    document.getElementById("title-id").value = "";
+    document.getElementById("destinataire-id").value = "";
+    document.getElementById("content-id").value = "";
+    alert("Message envoyé avec succès");
 
-        localStorage.setItem(value,emailstr)
-        value++
-        localStorage.nombre=value
-        let displaytest = JSON.parse(localStorage.getItem(1))
-    
-        loadnew()
-    }
+    let email = {};
+    email.titre = titre_txt;
+    email.destinataire = receiver_txt;
+    email.contenu = content_txt;
+    let emailstr = JSON.stringify(email);
+    console.log(emailstr);
 
+    let value = localStorage.nombre;
+
+    localStorage.setItem(value, emailstr);
+    value++;
+    localStorage.nombre = value;
+    let displaytest = JSON.parse(localStorage.getItem(1));
+
+    loadnew();
+  }
 }
 /* fonction permetant de créer la ''card'' du nouveau courriel */
-function loadnew(){
-    const nombre = localStorage.nombre
-    let emailload = JSON.parse(localStorage.getItem(nombre-1))
-    console.log(nombre)
-    const card = userCardTemplate.content.cloneNode(true).children[0];
-    const header = card.querySelector("[data-header]");
-    const body = card.querySelector("[data-body]");
-    const content = card.querySelector("[data-content]");
-    header.textContent = emailload.titre;
-    body.textContent = emailload.destinataire;
-    content.textContent = emailload.contenu;
-    userCardContainer.append(card);
-    return { name: header.textContent, email: body.textContent, content: content.textContent ,element: card };
+function loadnew() {
+  const nombre = localStorage.nombre;
+  let emailload = JSON.parse(localStorage.getItem(nombre - 1));
+  console.log(nombre);
+  const card = userCardTemplate.content.cloneNode(true).children[0];
+  const header = card.querySelector("[data-header]");
+  const body = card.querySelector("[data-body]");
+  const content = card.querySelector("[data-content]");
+  header.textContent = emailload.titre;
+  body.textContent = emailload.destinataire;
+  content.textContent = emailload.contenu;
+  userCardContainer.append(card);
+  return {
+    name: header.textContent,
+    email: body.textContent,
+    content: content.textContent,
+    element: card,
+  };
 }
-
-
