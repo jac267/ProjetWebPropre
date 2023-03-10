ec0e4e12a95b8494930cdcd59cf023428350a86f
CA-WillMc <122403149+CA-WillMc@users.noreply.github.com>
2023-03-10 18:30:57 -0500

    liste email fix

--- script/listeEmail.js
+++ script/listeEmail.js
@@ -3,21 +3,24 @@ const userCardContainer = document.querySelector("[data-user-cards-container]");
 const searchInput = document.querySelector("[data-search]");
 
 let users = [];
+
+/*
 for (let i = 0; i < localStorage.nombre; i++) {
   let temp = JSON.parse(localStorage.getItem(i));
   let tempArray = [];
   tempArray.push(temp.titre);
   tempArray.push(temp.destinataire);
+  tempArray.push(card);
   users.push(tempArray);
 }
+*/
 searchInput.addEventListener("input", (e) => {
   const value = e.target.value.toLowerCase();
   users.forEach((user) => {
     const isVisible =
-      user[0].toLowerCase().includes(value) ||
-      user[1].toLowerCase().includes(value);
-    let toggleTemp = document.getElementById(`${users.indexOf(user)}`);
-    toggleTemp.classList.toggle("hide", !isVisible);
+      user.name.toLowerCase().includes(value) ||
+      user.email.toLowerCase().includes(value);
+    user.element.classList.toggle("hide", !isVisible);
   });
 });
 
@@ -41,9 +44,8 @@ function addEmail() {
 /* Fonction qui va créer la loop pour recréer la ''card'' de chaque courriel lorsqu'on charche la page */
 function loadinit() {
   const nombre = localStorage.nombre;
-  console.log(nombre);
   for (var i = 0; i < nombre; i++) {
-    emailcreate(i);
+    users.push(emailcreate(i));
   }
 }
 /* Fonction qui s'occupe de spécifiquement de créer une seule ''card'' , elle doit être utilisé avec loadinit() pour bien fonctionner */
